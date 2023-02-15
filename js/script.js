8705bcf25735b719f310fb81a23844f6ef3947e8
Your Name <anneserocki@gmail.comom>
2023-02-14 21:52:39 -0800

    initial

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Step1: Convert the following highlighted identifiers to Camel Case notation
+let someMonth
+function theMonth()
+let currentMonth
+let summerMonth
+let myLibraryFunction
+
+// Step2: 
